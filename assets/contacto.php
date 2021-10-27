fd4fc8075a0614d5f47f4eeb3f2d6c4ab70dcd93
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2021-10-26 21:19:39 -0500

    [TlahuicoleSystem] Captcha y correcion de formulario

---
+++ assets/contacto.php
@@ -0,0 +1,25 @@
+<?php
+
+define("RECAPTCHA_V3_SECRET_KEY", '6LdO6vYcAAAAAMBkoJMs96byiAe4pEBwIZblEFNB');
+
+$token = $_POST['token'];
+$action = $_POST['action'];
+ 
+// call curl to POST request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$arrResponse = json_decode($response, true);
+ 
+// verificar la respuesta
+if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+    // Procesar el formulario xDD
+	echo "ok!, eres un humano";
+} else {
+    // Fallo el captcha
+	echo "Lo siento, parece que eres un Robot";
+}
